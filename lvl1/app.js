ec93e43b734ceab2c013f2f34ba97973abd81238
AhmedUmanlink <ahmed.jaidi@umanlink.com>
2024-05-06 12:21:18 +0100

    c3: setting up mongo db + connection

--- lvl1/app.js
+++ lvl1/app.js
@@ -1,17 +1,23 @@
-//imports 
+//imports
 
-const express = require('express')
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile("views/home.html", { root: __dirname });
+});
 
 
-
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-    res.sendFile('views/home.html', { root: __dirname });
-})
-
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/`)
-})
+mongoose
+  .connect("mongodb+srv://umanlink61:iec2LHD1LbBHJpko@cluster0.3c4rryz.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0")
+  .then(() => {
+    app.listen(port, () => {
+        console.log(`http://localhost:${port}/`);
+      });
+cls      
+  })
+  .catch((err) => {console.error(err);});
