6153b5d63de4db5c592ede45f591c411953ae434
AhmedUmanlink <ahmed.jaidi@umanlink.com>
2024-05-06 11:53:55 +0100

    c2 : sending html file

---
+++ lvl1/app.js
@@ -0,0 +1,17 @@
+//imports 
+
+const express = require('express')
+const path = require('path');
+
+
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.sendFile('views/home.html', { root: __dirname });
+})
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}/`)
+})
