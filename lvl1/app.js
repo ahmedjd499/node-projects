d6f8bb092c8298002abe99423e768d33e20e17ca
AhmedUmanlink <ahmed.jaidi@umanlink.com>
2024-05-06 17:57:46 +0100

    adding template bootstrap +adjusting routes

--- lvl1/app.js
+++ lvl1/app.js
@@ -7,59 +7,42 @@ const User = require("./models/user");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'))
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 const port = 3000;
 //// auto refresh
-const path =require('path')
-const livereload =require('livereload')
-const liveReloadServer=livereload.createServer()
-liveReloadServer.watch(path.join(__dirname,'public'))
+const path = require("path");
+const livereload = require("livereload");
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, "public"));
 
-const connectLiveReload = require('connect-livereload')
-app.use(connectLiveReload())
+const connectLiveReload = require("connect-livereload");
+app.use(connectLiveReload());
 
-liveReloadServer.server.once("connection",()=>{
-    setTimeout(()=>{
-        liveReloadServer.refresh('/')
-    },100)
-})
-
-
-
-
-
-
-
-
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
 
 app.get("/", (req, res) => {
-  User.find()
-    .then((result) => {
-      res.render("home", { title: "Home page", users: result });
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+  res.render("index", { title: "Home page" });
 });
 
-app.get("/index.html", (req, res) => {
-  res.send("<h1>user saved ✔</h1><br><a href='/'>◀ return </a>");
+app.get("/user/add.html", (req, res) => {
+  res.render("user/add");
+});
+app.get("/user/edit.html", (req, res) => {
+  res.render("user/edit");
+});
+app.get("/user/view.html", (req, res) => {
+  res.render("user/view");
 });
 
-app.post("/add/user", (req, res) => {
-  console.log(req.body);
-  const user = new User(req.body);
-  user
-    .save()
-    .then(() => {
-      console.log("user saved successfully");
-      res.redirect("/index.html");
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+app.get("/user/search.html", (req, res) => {
+  res.render("user/search");
 });
+
 mongoose
   .connect(
     "mongodb+srv://umanlink61:iec2LHD1LbBHJpko@cluster0.3c4rryz.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0"
