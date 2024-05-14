0153b81b840c6cf099d8cc6a7004768cc1f54df8
AhmedUmanlink <ahmed.jaidi@umanlink.com>
2024-05-14 11:11:26 +0100

    c13:  search query (filter )

--- lvl1/app.js
+++ lvl1/app.js
@@ -3,8 +3,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const User = require("./models/user");
-const moment=require("moment")
-var methodOverride = require('method-override')
+const moment = require("moment");
+var methodOverride = require("method-override");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -19,7 +19,7 @@ liveReloadServer.watch(path.join(__dirname, "public"));
 
 const connectLiveReload = require("connect-livereload");
 app.use(connectLiveReload());
-app.use(methodOverride('_method'))
+app.use(methodOverride("_method"));
 
 liveReloadServer.server.once("connection", () => {
   setTimeout(() => {
@@ -28,79 +28,109 @@ liveReloadServer.server.once("connection", () => {
 });
 
 app.get("/", (req, res) => {
-  User.find().then((result)=>{
-   // console.log(result);
-    res.render("index", { title: "Home page" ,users : result,moment:moment});
-
-  }).catch((err)=>{
-    res.redirect("/somethingwhentwrong");
-    console.log(err);});
+  User.find()
+    .then((result) => {
+      // console.log(result);
+      res.render("index", {
+        title: "Home page",
+        users: result,
+        moment: moment,
+      });
+    })
+    .catch((err) => {
+      res.redirect("/somethingwhentwrong");
+      console.log(err);
+    });
 });
 
 app.get("/somethingwhentwrong", (req, res) => {
- 
-    res.render("404");
-}
-  )
+  res.render("404");
+});
 
 app.get("/user/add.html", (req, res) => {
   res.render("user/add");
 });
 app.get("/user/edit/:id", (req, res) => {
-  console.log(req.params.id); 
-  User.findById(req.params.id).then((result)=>{
-console.log(result);
-    res.render("user/edit",{user : result});
-  }).catch((err)=>{
-    res.redirect("/somethingwhentwrong");
-    console.log(err);})
+  console.log(req.params.id);
+  User.findById(req.params.id)
+    .then((result) => {
+      console.log(result);
+      res.render("user/edit", { user: result });
+    })
+    .catch((err) => {
+      res.redirect("/somethingwhentwrong");
+      console.log(err);
+    });
 });
 
 app.get("/user/:id", (req, res) => {
- console.log(req.params.id); 
-  User.findById(req.params.id).then((result)=>{
-console.log(result);
-    res.render("user/view",{user : result,moment:moment});
-  }).catch((err)=>{
-    res.redirect("/somethingwhentwrong");
-    console.log(err);})
+  console.log(req.params.id);
+  User.findById(req.params.id)
+    .then((result) => {
+      console.log(result);
+      res.render("user/view", { user: result, moment: moment });
+    })
+    .catch((err) => {
+      res.redirect("/somethingwhentwrong");
+      console.log(err);
+    });
 });
 
 app.get("/user/search.html", (req, res) => {
   res.render("user/search");
 });
 
-
-
 app.post("/user/add", (req, res) => {
-User.create(req.body).then(()=>{
-    console.log('user saved successfully');
-    
-res.redirect('/user/add.html')  }).catch((err)=>{console.log(err);})
+  User.create(req.body)
+    .then(() => {
+      console.log("user saved successfully");
+
+      res.redirect("/user/add.html");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
-
+app.post("/user/search", (req, res) => {
+  User.find({
+   $or:[{firstname : req.body.firstname},{lastname : req.body.firstname}]
+  })
+    .then((result) => {
+      console.log(result);
+      res.render("index", { title: "Search result" ,users : result,moment:moment});
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 app.delete("/user/delete/:id", (req, res) => {
-  console.log(req.params.id); 
-  User.findByIdAndDelete(req.params.id).then((result)=>{
-    console.log(result);
-    res.redirect("/");
-  }).catch((err)=>{
-    res.redirect("/somethingwhentwrong");
-    console.log(err);})
-})
+  console.log(req.params.id);
+  User.findByIdAndDelete(req.params.id)
+    .then((result) => {
+      console.log(result);
+      res.redirect("/");
+    })
+    .catch((err) => {
+      res.redirect("/somethingwhentwrong");
+      console.log(err);
+    });
+});
 
 app.put("/user/editOne/:id", (req, res) => {
-  console.log(req.params.id); 
-  console.log(req.body); 
-  User.findByIdAndUpdate(req.params.id,req.body).then((result)=>{
-   //console.log(result);
-    res.redirect(`/user/edit/${req.params.id}`);
-  }).catch((err)=>{
-    res.redirect("/somethingwhentwrong");
-    console.log(err);})
-})
+  console.log(req.params.id);
+  console.log(req.body);
+  User.findByIdAndUpdate(req.params.id, req.body)
+    .then((result) => {
+      //console.log(result);
+      res.redirect(`/user/edit/${req.params.id}`);
+    })
+    .catch((err) => {
+      res.redirect("/somethingwhentwrong");
+      console.log(err);
+    });
+});
 mongoose
   .connect(
     "mongodb+srv://umanlink61:iec2LHD1LbBHJpko@cluster0.3c4rryz.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0"
